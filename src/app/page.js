5bc99fa77fd865edbde6a81c552d4d4457069e8a
Kevin Binu Thottumkal <kevinthottumkal11@gmail.com>
2024-10-27 09:33:58 -0400

    tetris audio added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,21 @@
+"use client"
 import "./styles.css";
+import { useRef } from "react";
 
 import Game from "/src/components/Game";
 
 export default function App() {
+
+  const audioRef = useRef();
+
+  const play = () => {
+    if (audioRef.current) {
+      audioRef.current.play()
+    }
+  }
   return (
-    <div className="App">
+    <div className="App" onClick={play}>
+      <audio ref={audioRef} src='/assets/tetris_99.mp3' />
       <Game rows={20} columns={10} />
     </div>
   );
